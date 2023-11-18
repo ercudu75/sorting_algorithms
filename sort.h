4fe91e59ea451a077a44324dbfab580153ef1ab4
ercudu <ilyass.bentalha2@gmail.com>
2023-11-18 14:14:07 +0000

    add task 3

--- sort.h
+++ sort.h
@@ -24,6 +24,9 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void quicksort(int *array, int low, int heigh, size_t size);
+int partition(int *array, int low, int high, size_t size);
+void swap(int *array, size_t a, size_t b);
 
 #endif
